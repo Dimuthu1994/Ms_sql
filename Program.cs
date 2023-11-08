08255ea6942859fbda59f5f197532932d57c1beb
ddimuthu <148847546+ddimuthu@users.noreply.github.com>
2023-11-08 11:39:47 +0530

    first instance of our first model

--- Program.cs
+++ Program.cs
@@ -2,10 +2,30 @@
 
 namespace HelloWorld
 {
+    public class Computer
+    {
+        public string Motherboard{get; set;} = "";
+        public int CPUCores{get; set;}
+        public bool HasWifi{get; set;}
+        public bool HasLTE{get; set;}
+        public DateTime ReleaseDate{get; set;}
+        public decimal Price{get; set;}
+        public string? VideoCard{get; set;} = "";
+
+    }
     internal class Program{
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Computer myComputer = new Computer()
+            {
+                Motherboard = "z690",
+                HasWifi = true,
+                HasLTE = false,
+                ReleaseDate = DateTime.Now,
+                Price = 100.89m,
+                VideoCard = "RTX 2060"
+
+            };
         }
     }
 }
