b1b850576e1912e7ff621cab9cfd3c9a316608dc
ddimuthu <148847546+ddimuthu@users.noreply.github.com>
2023-11-08 12:22:30 +0530

    connect to the db and run a simple query

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Data;
+using Dapper;
 using HelloWorld.Models;
+using Microsoft.Data.SqlClient;
 
 namespace HelloWorld
 {
@@ -7,6 +10,13 @@ namespace HelloWorld
     internal class Program{
         static void Main(string[] args)
         {
+            string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;";
+            IDbConnection dbConnection = new SqlConnection(connectionString);
+
+            string sqlCommand = "SELECT GETDATE()";
+            DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+            Console.WriteLine(rightNow);
+            
             Computer myComputer = new Computer()
             {
                 Motherboard = "z690",
